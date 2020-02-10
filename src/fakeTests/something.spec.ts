9bc7ab6e2dd4dbbb3fdf6124a9aa89179b58bc0e
Giuliano Kranevitter <info@giulianok.com>
2020-02-10 13:10:33 -0500

    ts fix

--- src/fakeTests/something.spec.ts
+++ src/fakeTests/something.spec.ts
@@ -1,18 +1,12 @@
-import something from "./something";
-const hi = test(
-  'returns "che!"',
-  () => {
-    expect(something(15)).toBe("che!");
-  },
-  "something"
-).withMetaData("something", {
-    documentation: "this is some test docs",
-    tags: ["tag1", "tag2"]
-  });
+import something from './something';
 
-test(
-  'returns "blah"',
-  () => {
-    expect(something(7)).toBe("blah");
-  }
-);
+test('returns "che!"', () => {
+  expect(something(15)).toBe('che!');
+}).withMetaData('something', {
+  documentation: 'this is some test docs',
+  tags: ['tag1', 'tag2']
+});
+
+test('returns "blah"', () => {
+  expect(something(7)).toBe('blah');
+});
--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,10 @@
-import deepmerge from "deepmerge";
+import deepmerge from 'deepmerge';
 
 export default {
-    withMetaData: function (cb: Function) {
-        return function(fileName, metaData: any) {
-            (this as any).metaData = deepmerge((this as any).metaData, metaData);
-            cb && cb();
-        }
-    }
+  withMetaData: function(cb: Function) {
+    return function(_fileName: string, metaData: any) {
+      (this as any).metaData = deepmerge((this as any).metaData, metaData);
+      cb && cb();
+    };
+  }
 };
