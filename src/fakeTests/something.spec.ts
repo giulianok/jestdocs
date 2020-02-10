855c2c05aadc38b5c0a5c6d81df7fbf95d5181b3
Ryan Byrne <ryanabyrne@gmail.com>
2020-02-10 11:24:04 -0500

    Add metadata function

--- src/fakeTests/something.spec.ts
+++ src/fakeTests/something.spec.ts
@@ -1,27 +1,32 @@
-import something from './something';
+import something from "./something";
 test(
   'returns "che!"',
   () => {
-    expect(something(15)).toBe('che!');
+    expect(something(15)).toBe("che!");
   },
   {
     metaData: {
       fileName: "something",
-      tags: ['code', 'stuff', 'useless']
+      tags: ["code", "stuff", "useless"]
     },
     aloja: true
   }
-);
+)
+  // .withTags(["addition, argentina"])
+  .withMetaData({
+    documentation: "this is some test docs",
+    tags: ["tag1", "tag2"]
+  });
 
 test(
   'returns "blah"',
   () => {
-    expect(something(7)).toBe('blah');
+    expect(something(7)).toBe("blah");
   },
   {
     metaData: {
       fileName: "something",
-      tags: ['more', 'stuff', 'here']
+      tags: ["more", "stuff", "here"]
     },
     something: true
   }
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,26 +1,33 @@
-import '@testing-library/jest-dom/extend-expect';
-import union from "lodash.union"
+import "@testing-library/jest-dom/extend-expect";
 import deepmerge from "deepmerge";
+import utils from "../src/utils";
 
 const jestTest = (global as any).test;
-let collection = {};
+
+let collection = {} as any;
 
 if (jestTest != null) {
   const newTest = (description: string, cb: Function, data: any) => {
     jestTest(description, cb);
     const fileName = data?.metaData?.fileName || "global";
-    if(!collection[fileName]) {
+    if (!collection[fileName]) {
       collection[fileName] = data;
       collection[fileName].tests = [];
     } else {
-      collection[fileName].metaData = deepmerge(collection[fileName].metaData, data.metaData);
+      collection[fileName].metaData = deepmerge(
+        collection[fileName].metaData,
+        data.metaData
+      );
     }
     collection[fileName].tests.push({
       type: "test",
       code: cb.toString(),
       metadata: data.metaData
     });
-    console.log(collection)
+
+    collection.withMetaData = utils.withMetaData;
+    console.log(collection);
+    return collection;
   };
   (global as any).test = newTest;
 } else {
