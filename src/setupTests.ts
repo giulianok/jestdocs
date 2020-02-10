d6f64ea82c309db0072a15e1655a8ab407a9e91f
Bryan Charlie <bcharlie1886@gmail.com>
2020-02-10 11:51:58 -0500

    added filepath

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -5,18 +5,25 @@ import deepmerge from "deepmerge";
 const glob = require("glob");
 const jestTest = (global as any).test;
 let collection = {};
+let graph = {};
 
-const collectData = () => {
-  Object.keys()
+const getFilePath = (fileName) => {
+  let files = glob.sync(`**/${fileName}*.spec.ts`);
 }
 
 if (jestTest != null) {
-  const newTest = (description: string, cb: Function, data: any) => {
+  const newTest = (description: string, cb: Function, data: any = {
+    metadata: {}
+  }) => {
     jestTest(description, cb);
     const fileName = data?.metaData?.fileName || "global";
     if(!collection[fileName]) {
       collection[fileName] = data;
       collection[fileName].tests = [];
+      collection[fileName].metadata = {
+        ...(collection[fileName].metadata || {}),
+        filePath: getFilePath(fileName)
+      };
     } else {
       collection[fileName].metaData = deepmerge(collection[fileName].metaData, data.metaData);
     }
